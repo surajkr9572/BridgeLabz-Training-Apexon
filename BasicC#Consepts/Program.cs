60e7cbc08f1616361471e5ffaddf1d30d9f26f55
surajkr9572 <surajsingh43006@gmail.com>
2025-12-02 13:08:27 +0530

    created the project for basic c#

---
+++ BasicC#Consepts/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BasicC_Consepts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
