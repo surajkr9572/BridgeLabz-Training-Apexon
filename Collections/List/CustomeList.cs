023038d3e3465c69a842486ad795766394368d9d
surajkr9572 <surajsingh43006@gmail.com>
2025-12-19 10:52:20 +0530

    CustomeList

--- Collections/List/CustomeList.cs
+++ Collections/List/CustomeList.cs
@@ -6,7 +6,63 @@ using System.Threading.Tasks;
 
 namespace Collections.List
 {
-    internal class CustomeList
+    public class CustomeList<T>
     {
+        private T[] items;
+        private int count;
+        public CustomeList()
+        {
+            items = new T[4];
+            count = 0;
+        }
+        public int Count
+        {
+            get {  return count; }
+        }
+        public void Add(T item)
+        {
+            if(count==items.Length)
+            {
+                Resize();
+            }
+            items[count] = item;
+            count++;
+        }
+        private void Resize()
+        {
+            T[] newArray = new T[items.Length * 2];
+            for(int i=0;i<items.Length;i++)
+            {
+                newArray[i] = items[i];
+
+            }
+            items = newArray;
+        }
+        public void print()
+        {
+            for(int i = 0; i < count; i++)
+            {
+                Console.Write(items[i]+" ");
+            }
+            Console.WriteLine();
+        }
+
+        public void Remove(int index)
+        {
+            if(index<0 || index >= count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else
+            {
+                for(int i =index;i<count-1 ; i++)
+                {
+                    items[i] = items[i + 1];
+                }
+                items[count-1] = default(T);
+                count --;
+            }
+
+        }
     }
 }
--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Collections
+﻿using Collections.List;
+
+namespace Collections
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            CustomeList<int> numbers = new CustomeList<int>();
+
+            numbers.Add(10);
+            numbers.Add(20);
+            numbers.Add(30);
+            numbers.Add(40);
+            numbers.Add(50); 
+
+            Console.WriteLine("Count: " + numbers.Count);
+
+            numbers.print();
+            numbers.Remove(2);
+            numbers.print();
         }
     }
 }
