f773e43d39febd87130e829e0ce04bbbda2e8407
surajkr9572 <surajsingh43006@gmail.com>
2025-12-23 12:58:40 +0530

    Delegate

---
+++ Delegates/PredicateImp/PredicateWithLambda.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates.PredicateImp
+{
+     class PredicateWithLambda
+    {
+        public bool Even(int num)
+        {
+            Predicate<int> pr = n => n % 2 == 0;
+            return pr(num);
+        }
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,14 @@
+﻿using Delegates.PredicateImp;
+
+namespace Delegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PredicateWithLambda pwl = new PredicateWithLambda();
+            Console.WriteLine( pwl.Even(5));
+            Console.WriteLine(pwl.Even(6));
+        }
+    }
+}
