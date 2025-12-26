c2a0df336ab7d518b8b48b196cdbce219a25393f
surajkr9572 <surajsingh43006@gmail.com>
2025-12-27 00:52:12 +0530

    Using both StreamReader and StreamWriter

--- FileHandling/Program.cs
+++ FileHandling/Program.cs
@@ -33,18 +33,92 @@ namespace FileHandling
             //Example to Write User Input to a File using StreamWriter Class
 
 
-            StreamWriter streamWriter = new StreamWriter(path);
-            // Asking the user to enter the text that we want to write into the MyFile.txt file
-            Console.WriteLine("Enter the Text that you want to write on File");
-            // To read the input from the user
-            string inputData = Console.ReadLine();
-            // To write the data into the stream use the Write Method of the StreamWriter Object
-            streamWriter.Write(inputData);
-            Console.WriteLine("Data Has Been Written to the File");
-            // Clears all the buffers for the current writer by calling the Flush Method of the StreamWriter Object
-            streamWriter.Flush();
-            // Close the current StreamWriter object and the underlying stream by calling the Flush Method of the StreamWriter Object
-            streamWriter.Close();
+            //StreamWriter streamWriter = new StreamWriter(path);
+            //// Asking the user to enter the text that we want to write into the MyFile.txt file
+            //Console.WriteLine("Enter the Text that you want to write on File");
+            //// To read the input from the user
+            //string inputData = Console.ReadLine();
+            //// To write the data into the stream use the Write Method of the StreamWriter Object
+            //streamWriter.Write(inputData);
+            //Console.WriteLine("Data Has Been Written to the File");
+            //// Clears all the buffers for the current writer by calling the Flush Method of the StreamWriter Object
+            //streamWriter.Flush();
+            //// Close the current StreamWriter object and the underlying stream by calling the Flush Method of the StreamWriter Object
+            //streamWriter.Close();
+            //Console.ReadKey();
+
+            //Save Variable Data to File in C# using StreamWriter Class
+
+            //StreamWriter streamWriter=new StreamWriter(path);
+            //Console.WriteLine("Enter two digit: ");
+            //int a=int.Parse(Console.ReadLine());
+            //int b=int.Parse(Console.ReadLine());
+            //int c = a + b;
+            //streamWriter.Write(c);
+            //Console.WriteLine("Data has been written in file");
+            //streamWriter.Flush();
+            //Console.WriteLine(c);
+
+            //Save Variable Data to File in C# using StreamWriter Class
+            //Console.WriteLine("Enter two digit : ");
+            //int a=int.Parse(Console.ReadLine());
+            //int b=int.Parse(Console.ReadLine());
+            //int result = a + b;
+            //using(StreamWriter streamWriter=new StreamWriter(path))
+            //{
+            //    streamWriter.Write($"The Sum of {a} and {b} = {result}");
+
+            //}
+            //Console.WriteLine("Variable Data is already saved");
+
+            //Console.WriteLine("Enter two digit : ");
+            //int a = int.Parse(Console.ReadLine());
+            //int b = int.Parse(Console.ReadLine());
+            //int result = a + b;
+            //using (StreamWriter streamWriter = new StreamWriter(path,true))
+            //{
+            //    streamWriter.Write($", The Sum of {a} and {b} = {result}");
+
+            //}
+            //Console.WriteLine("Variable Data is already saved");
+
+
+
+            //StreamReader Class in C#
+            //Example to Read Data from a File using StreamReader Class
+
+
+            //Console.WriteLine("Contant of the file");
+            //using (StreamReader reader = new StreamReader(path))
+            //{
+            //    Console.WriteLine(reader.ReadToEnd());
+            //}
+
+            //Using both StreamReader and StreamWriter Class in C#
+            using (StreamWriter streamWriter = new StreamWriter(path))
+            {
+                streamWriter.WriteLine("Welcome to DotNetTutorials");
+                streamWriter.WriteLine("You are Learning File Handling in C#");
+            }
+
+            //Reading text file using StreamReader Class ReadToEnd Method
+            Console.WriteLine("\nReading Approach 1: using ReadToEnd Method");
+            using (StreamReader reader = new StreamReader(path))
+            {
+                Console.WriteLine(reader.ReadToEnd());
+            }
+
+            //Another Approach to Read the Data from a text file using StreamReader ReadLine Method
+            Console.WriteLine("\nReading Approach 2: using ReadLine Method");
+            StreamReader streamReader = new StreamReader(path);
+            streamReader.BaseStream.Seek(0, SeekOrigin.Begin);
+            string strData = streamReader.ReadLine();
+            while (strData != null)
+            {
+                Console.WriteLine(strData);
+                strData = streamReader.ReadLine();
+            }
+
             Console.ReadKey();
         }
     }
--- FileHandling/obj/Release/net8.0/FileHandling.AssemblyInfo.cs
+++ FileHandling/obj/Release/net8.0/FileHandling.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileHandling")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f978c2fc100f16c494f3fffdc5ca4f1856db60f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileHandling")]
