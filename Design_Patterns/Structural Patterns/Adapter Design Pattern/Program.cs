d866df09feeec3264a6403b49eeb28b81f4fb011
surajkr9572 <surajsingh43006@gmail.com>
2025-12-31 11:30:46 +0530

    Adapter Design Pattern

---
+++ Design_Patterns/Structural Patterns/Adapter Design Pattern/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Adapter_Design_Pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var unknown = new Compound();
+            unknown.Display();
+            var water = new RichCompound(Chemical.Water);
+            water.Display();
+            var benzene = new RichCompound(Chemical.Benzene);
+            benzene.Display();
+            var ethanol = new RichCompound(Chemical.Ethanol);
+            ethanol.Display();
+
+        }
+    }
+}
