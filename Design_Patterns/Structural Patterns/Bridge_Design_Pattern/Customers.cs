3d85d0484481c1ff922e6819c904bac0b4d892f5
surajkr9572 <surajsingh43006@gmail.com>
2025-12-31 11:09:41 +0530

    Structural code

---
+++ Design_Patterns/Structural Patterns/Bridge_Design_Pattern/Customers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge_Design_Pattern
+{
+    public class Customers(IDataObject<string>dataObject):CustomersBase(dataObject)
+    {
+        public override void ShowAll()
+        {
+            Console.WriteLine();
+            Console.WriteLine("-----------------------------");
+            base.ShowAll();
+            Console.WriteLine("-----------------------------");
+        }
+    }
+}
---
+++ Design_Patterns/Structural Patterns/Bridge_Design_Pattern/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Bridge_Design_Pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var customers = new Customers(new CustomersData("Chicago"));
+            customers.Show();
+            customers.Next();
+            customers.Show();
+            customers.Next();
+            customers.Show();
+            customers.Add("Ram");
+            customers.ShowAll();
+
+        }
+    }
+}
